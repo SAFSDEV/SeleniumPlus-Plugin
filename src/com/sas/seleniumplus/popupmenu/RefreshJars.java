e7f5ed4f388b9d187a0b3865a62b4df8722bdc24
sbjlwa <lei.wang@sas.com>
2017-04-25 16:56:37 +0800

    The menu "Refresh Build Path" will firstly download source code if the check-box "source code" in the preference page "Update Site" is checked.

--- src/com/sas/seleniumplus/popupmenu/RefreshJars.java
+++ src/com/sas/seleniumplus/popupmenu/RefreshJars.java
@@ -1,13 +1,24 @@
 package com.sas.seleniumplus.popupmenu;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+
+import com.sas.seleniumplus.Activator;
+import com.sas.seleniumplus.CommonLib;
 public class RefreshJars extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		//TODO we should update the source code firstly
+		try {
+			int updatedFileNumber = CommonLib.updateSource(Activator.seleniumhome, CommonLib.getUpdateTimeout());
+			Activator.log("Updated "+updatedFileNumber+" source files.");
+		} catch (URISyntaxException | IOException e) {
+			Activator.warn("Failed to update source code!");
+		}
 		CommonLib.refreshBuildPath();
 		return null;
 	}
